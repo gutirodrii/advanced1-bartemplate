929e95e5f7e627c67c2dbb8bb7982a44999cc411
gutirodrii <gutirodriidev@gmail.com>
2025-01-16 11:48:51 +0100

    first structure

---
+++ src/app/components/SectionOne.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SectionOne() {
+  return (
+    <div className='flex w-full items-center justify-center'>SectionOne</div>
+  )
+}
+
+export default SectionOne
---
+++ src/app/components/Separator.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Separator() {
+  return (
+    <div className='w-full h-1 bg-gray-200 my-4'></div>
+  )
+}
+
+export default Separator
